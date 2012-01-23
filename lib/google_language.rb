606d371b76f8dd24d1e385dd12295cca149c8181
Hector Bustillos <hector.bustillos@crowdint.com>
2012-01-23 16:24:29 -0600

    change google lang class to use v2 api

--- lib/google_language.rb
+++ lib/google_language.rb
@@ -6,28 +6,25 @@ class GoogleLanguage
 
   # Thanks http://ruby.geraldbauer.ca/google-translation-api.html
   def self.translate( text, to, from='en' )
-
-    base = 'http://ajax.googleapis.com/ajax/services/language/translate' 
+    base = 'https://www.googleapis.com/language/translate/v2'
 
     # assemble query params
     params = {
-      :langpair => "#{from}|#{to}", 
+      :source => "#{from}",
+      :target => "#{to}",
       :q => text,
-      :v => 1.0  
+      :key => # add your google app key
     }
 
     query = params.map{ |k,v| "#{k}=#{CGI.escape(v.to_s)}" }.join('&')
 
     # send get request
-    response = Net::HTTP.get_response( URI.parse( "#{base}?#{query}" ) )
+    # response = Net::HTTP.get_response( URI.parse( "#{base}?#{query}" ) )
+    puts "URI >> #{URI.parse( "#{base}?#{query}")}"
+    response = JSON.parse(open(URI.parse( "#{base}?#{query}" )).read)
 
-    json = JSON.parse( response.body )
+    response['data']['translations'].first['translatedText']
 
-      if json['responseStatus'] == 200
-        json['responseData']['translatedText']
-      else
-        raise StandardError, response['responseDetails']
-      end
   end
 
 end
